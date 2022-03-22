686a7b08bff6479662903765040eea51773eb261
vinicius216 <vinicius2612@hotmail.com>
2022-03-21 22:20:30 -0300

    Exercicio 3 lista 6

--- lista3/exercicio1.py
+++ lista3/exercicio1.py
@@ -2,9 +2,30 @@
 
 # Exercício 1
 
-alunos_ingles = {"João Alves dos Santos" , "Maria Magalhães", "Antônio da Silva Ferreira", "José Júnior Jarbas", "Henrique da Silva Sauro", "Joaquina Ferreira da Silva", "Fabiana Aparecida Bianco", "Marrone Gutierres", "Carlos Magno Farad", "Antônio da Silva Júnior Amaral"}
+alunos_ingles = {
+    "João Alves dos Santos" ,
+    "Maria Magalhães",
+    "Antônio da Silva Ferreira",
+    "José Júnior Jarbas",
+    "Henrique da Silva Sauro",
+    "Joaquina Ferreira da Silva", 
+    "Fabiana Aparecida Bianco", 
+    "Marrone Gutierres", 
+    "Carlos Magno Farad", 
+    "Antônio da Silva Júnior Amaral"
+}
 
-alunos_frances = {"João Alves dos Santos", "Antônio da Silva Ferreira", "Fernanda Abdala Mohamed", "Abner Mignon Alib", "Alisson Figueiredo", "Henrique da Silva Sauro","Maria Magalhães", "Marrone Gutierres", "Joaquina Ferreira da Silva"}
+alunos_frances = {
+    "João Alves dos Santos", 
+    "Antônio da Silva Ferreira", 
+    "Fernanda Abdala Mohamed", 
+    "Abner Mignon Alib", 
+    "Alisson Figueiredo", 
+    "Henrique da Silva Sauro",
+    "Maria Magalhães", 
+    "Marrone Gutierres", 
+    "Joaquina Ferreira da Silva"
+}
 
 total_de_alunos = len(alunos_ingles.union(alunos_frances))
 
@@ -18,3 +39,6 @@ print(f"\nEstão matriculados apenas em francês {len(alunos_frances-alunos_ingl
 print(f"\nEstão matriculados em ambos os curos {len(alunos_ingles.intersection(alunos_frances))} alunos, sendo eles : {alunos_ingles.intersection(alunos_frances)}")
 
 print(f"\nEstão matriculados em apenas um curso {len(alunos_ingles.union(alunos_frances)-alunos_ingles.intersection(alunos_frances))} alunos, sendo eles : {alunos_ingles.union(alunos_frances)-alunos_ingles.intersection(alunos_frances)}")
+
+
+
---
+++ lista6/exercicio2.py
@@ -0,0 +1,30 @@
+#Lista de exercícios: Funções
+
+#Exercício 2
+
+from xmlrpc.client import boolean
+
+
+def triangulo_criterios (lado1: float, lado2: float, lado3: float) -> boolean:
+    """Verifica se as medidas dos lados possibilitam a formação de um triângulo."""
+    if lado1 ==0 or lado2 ==0 or lado3 ==0:
+        return False
+    elif lado1 + lado2 <= lado3 or lado2 + lado3 <= lado1 or lado1 + lado3 <= lado2:
+        return False    
+    else:
+         return True    
+
+
+Lado1 = float(input("Insira o primeiro lado do triângulo: "))
+
+Lado2 = float(input("Insira o segundo lado do triângulo: "))
+
+Lado3 = float(input("Insira o terceiro lado do triângulo: "))
+
+verificacao_triangulo = triangulo_criterios(Lado1, Lado2, Lado3)
+
+if verificacao_triangulo == True:
+    print("Esses lados podem formar um triângulo.")       
+
+else:
+    print("Esses lados não podem formar um triângulo.")
