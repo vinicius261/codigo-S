18a7a0f855693a525662c212888d73990977a213
vinicius216 <vinicius2612@hotmail.com>
2022-08-10 20:27:30 -0300

    reorganizando

--- codewars2/app.py
+++
@@ -1,8 +0,0 @@
-
-from flask import Flask
-from src.routes.cadastros import cadastros
-from src.routes.holetrite import holerite
-
-app = Flask(__name__)
-app.register_blueprint(cadastros)
-app.register_blueprint(holerite)
--- codewars2/src/execoes/base_cadastro_error.py
+++
@@ -1,6 +0,0 @@
-
-
-class BaseCadastroError(Exception):
-    def __init__(self, *args: object) -> None:
-        super().__init__(*args)
-    
